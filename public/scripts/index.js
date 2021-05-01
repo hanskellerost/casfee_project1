de18846c9c2bcf444578c76c838c408cda97b168
hanskeller <hans.keller@optimatik.ch>
2021-05-01 16:32:15 +0200

    chore: initial development

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+function readNotes() {
+    console.log('readNotes');
+}
+
+function readNote() {
+    console.log('readNote');
+}
+
+function createNote() {
+    console.log('createNote');
+}
+
+function updateNote() {
+    console.log('updateNote');
+}
+
+function deleteNote() {
+    console.log('deleteNote');
+}
